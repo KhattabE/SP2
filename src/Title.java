c8b9b23b4c001feb2b54a3abfd38101c34b17903
khat0076 <khattab2003@live.com>
2025-10-22 12:59:55 +0200

    Title Abstract class done

--- src/Title.java
+++ src/Title.java
@@ -1,6 +1,26 @@
 public abstract class Title {
 
+    //Tittle class attributes
+    private String title;
+    private String literatureType;
+    //The reason this attribute is final, is because it is a fixed RATE
+    private static final double RATE = 0.067574;
 
+    //Title class constructor
+    public Title(String title, String literatureType) {
+        this.title = title;
+        this.literatureType = literatureType;
+    }
+
+    //Method to calculate the royalties
+    public double calculateRoyalty(){
+        return 0;
+    }
+
+    //Abstract method that will(must) be implemented in the inheriting sub classes
+    public abstract double calculatePoints();
+
+    public abstract double convertLiteratureType();
 
 
 
