b867b0726dd01e8efeb611bf82aa8af2e5347f71
khat0076 <khattab2003@live.com>
2025-10-22 12:56:37 +0200

    Author class made

--- src/AudioBook.java
+++ src/AudioBook.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
+public class AudioBook extends Title{
 
-public class AudioBook {
+
+
+
 }
--- src/Author.java
+++ src/Author.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Author {
+
+
+    //Author class Attributes
+    private String name;
+    private ArrayList<Title> titles;
+
+
+    //Author Class constructor
+    public Author(String name) {
+        this.name = name;
+    }
+
+    //Getters
+    public String getName() {
+        return name;
+    }
+
+
+    //Method to add titles
+    public void addTitle(Title title){
+
+    }
+
+
+    public double calculateRoyalties(){
+        return 0;
+    }
+
+
+
+
+
+
+
+
 }
