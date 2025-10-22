580862502873f2f6cb67f7fba5dd8486d4b56c34
khat0076 <khattab2003@live.com>
2025-10-22 13:34:07 +0200

    PrintedBook class finished

--- src/PrintedBook.java
+++ src/PrintedBook.java
@@ -1,19 +1,18 @@
-public class AudioBook extends Title{
+public class PrintedBook extends Title{
 
-    //AudioBook class attributes
-    private int durationInMinutes;
+    //Class Attributes
+    private int pages;
     private int copies;
 
-    //Class Constructor
-    public AudioBook(String title, String literatureType, int durationInMinutes, int copies) {
-        //Super keyword so i can get the constructor from the parent class (Title)
+
+    //Constructor
+    public PrintedBook(String title, String literatureType, int pages, int copies) {
         super(title, literatureType);
-        this.durationInMinutes = durationInMinutes;
+        this.pages = pages;
         this.copies = copies;
     }
 
-
-    //The method will be used to check which type of literature the audio book is, and it will give points according to that
+    //The method will be used to check which type of literature the printed book is, and it will give points according to that
     //Override the abstract calculatePoints() from the parent class
     @Override
     public double convertLiteratureType() {
@@ -28,12 +27,14 @@ public class AudioBook extends Title{
     }
 
 
-    //The method for calculating the points of the audio books, with the use of the duration length and the copies of the audio book
+    //The method for calculating the points of the PrintedBook, with the use of the pages and the copies of the PrintedBook
     //Overriding the abstract calculatePoints() method from the super class
     @Override
     public double calculatePoints(){
-        return (durationInMinutes * 0.5 ) * convertLiteratureType() * copies;
+        return pages * convertLiteratureType() * copies;
     }
+
+
 
 
 }
